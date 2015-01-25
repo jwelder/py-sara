116ae4f14cae1aaf0a8d5d0f3bb570ef5dbf90b0
James Elder <James Elder@JamesElder-PC>
2015-01-25 15:06:49 -0700

    testing remember

--- sara/src/sara.py
+++ sara/src/sara.py
@@ -1,9 +1,42 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+from urllib.request import urlopen
 
-__author__ = "James Elder"
-__date__ = "$Jan 24, 2015 8:36:03 PM$"
+def document_it(func):
+    def new_function(*args, ** kwargs):
+        print('Running function: ', func.__name__)
+        print('Positional arguments: ', args)
+        print('Keyword arguments: ', kwargs)
+        result = func(*args, ** kwargs)
+        print('Result: ', result)
+        return result
+    return new_function
 
+@document_it
+def add_ints(a, b):
+    return a + b
+
+#print(add_ints(a = 3, b = 5))
+
+#cooler_add_ints = document_it(add_ints)
+#print(cooler_add_ints(3, 5))
+#    webUrl = urlopen("https://continue.utah.edu")
+#    print ("result code " + str(webUrl.getcode()))
+#    data = webUrl.read()
+#    print (data)
+
+from datetime import datetime
+
+def main(user):
+    print("My name is Sara, how can I be of assistance?")
+    fileobj = open('run-log.txt', 'a')
+    fileobj.write("Run " + str(user) + " @ " + str(datetime.now()) + "\n")
+    fileobj.close()
+    
+    
+
+    
+    
 if __name__ == "__main__":
-    print ("Hello " + __author__)
+    currentUser = "James Elder"
+    main(currentUser)   
+    
+    
---
+++ sara/src/setup.py
@@ -0,0 +1,27 @@
+__author__ = "James Elder"
+__date__ = "$Jan 25, 2015 3:01:33 PM$"
+
+from setuptools import setup, find_packages
+
+setup (
+       name='sara',
+       version='0.1',
+       packages=find_packages(),
+
+       # Declare your packages' dependencies here, for eg:
+       install_requires=['foo>=3'],
+
+       # Fill in these to make your Egg ready for upload to
+       # PyPI
+       author='James Elder',
+       author_email='',
+
+       summary='Just another Python package for the cheese shop',
+       url='',
+       license='',
+       long_description='Long description of the package',
+
+       # could also include long_description, download_url, classifiers, etc.
+
+  
+       )
